8288a2594dce3df503000fe54b4c35707e22c150
Nik <15204718+Menonn0228@users.noreply.github.com>
2021-07-22 10:22:11 -0400

    Added handlers for form changes.

--- galvanizeproject1/src/components/textfield.js
+++ galvanizeproject1/src/components/textfield.js
@@ -5,14 +5,43 @@ export default class Textfield extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            value: 0
+            incomeValue: 0,
+            taxValue: 0.00
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-// TODO: Include both textfields and the button in here.
+
+    handleChange(event) {
+        const target = event.target;
+        const value = parseInt(target.value)
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+
+        console.log(this.state)
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
-        return (<form>
-            <label for={this.props.valueType}>Annual Income: </label>
-            <input type="number" id={this.props.valueType} />
+        return (<form onSubmit={this.handleSubmit}>
+            <label>Annual Income: </label>
+            <input type="number" value={this.state.incomeValue}
+                name="incomeValue"
+                onChange={this.handleChange} />
+
+            <label>State Tax: </label>
+            <input type="number" value={this.state.taxValue}
+                name="taxValue"
+                onChange={this.handleChange} />
+
+            <input type="submit" value="Submit" />
         </form>
         )
     }
