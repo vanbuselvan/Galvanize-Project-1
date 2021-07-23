94ea9d3f76e4e3dece3f21ce2bbb3d9cb0732904
Nik <15204718+Menonn0228@users.noreply.github.com>
2021-07-23 11:53:46 -0400

    Added a Logo placeholder

--- galvanizeproject1/src/components/header.js
+++ galvanizeproject1/src/components/header.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import ReactIcon from '../static/ReactIcon.svg'
 import './Header.css';
 
 export default function header() {
     return (
-        <div class="Header">
-            <h1 class="Text">Partition Calculator</h1>
+        <div>
+            <nav class="navbar navbar-dark">
+                <strong>
+                    <a class="navbar-brand" href="#">
+                        <img src={ReactIcon}
+                            width="30"
+                            height="30"
+                            class="d-inline-block align-top"
+                            alt="" />
+                        Partition Calculator
+                    </a>
+                </strong>
+
+
+            </nav>
         </div>
     )
 }
